8a240ebf885b1faddb9d10529e6136b68c4422fc
preetyabhishek <theshivamsharma01@gmail.com>
2022-11-08 23:14:27 -0500

    Added initial
    Added initial

---
+++ Android_Assignment_2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Android_Assignment_2;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ Android_Assignment_2/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Android_Assignment_2;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
