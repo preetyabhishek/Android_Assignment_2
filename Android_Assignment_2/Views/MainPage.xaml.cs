e15d9692692862c62cc3f485419ce87250323a7c
preetyabhishek <theshivamsharma01@gmail.com>
2022-11-20 12:28:01 -0500

    committed some changes

--- Android_Assignment_2/Views/MainPage.xaml.cs
+++ Android_Assignment_2/Views/MainPage.xaml.cs
@@ -6,6 +6,11 @@ public partial class MainPage : ContentPage
     {
         InitializeComponent();
     }
+
+    private void InitializeComponent()
+    {
+        throw new NotImplementedException();
+    }
 }
 
 
